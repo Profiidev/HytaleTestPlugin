d205f9c8b21422f5c634f616927d359601dc11ca
ProviDev <92174452+Profiidev@users.noreply.github.com>
2026-01-15 22:26:51 +0100

    Renamed template to plugin name

--- src/main/java/io/profidev/HytaleTestPlugin/HytaleTestPlugin.java
+++ src/main/java/io/profidev/HytaleTestPlugin/HytaleTestPlugin.java
@@ -1,4 +1,4 @@
-package io.profidev.HytalePluginTemplate;
+package io.profidev.HytaleTestPlugin;
 
 import com.hypixel.hytale.logger.HytaleLogger;
 import com.hypixel.hytale.server.core.plugin.JavaPlugin;
@@ -6,11 +6,11 @@ import com.hypixel.hytale.server.core.plugin.JavaPluginInit;
 
 import javax.annotation.Nonnull;
 
-public class HytalePluginTemplate extends JavaPlugin {
+public class HytaleTestPlugin extends JavaPlugin {
 
     private static final HytaleLogger LOGGER = HytaleLogger.forEnclosingClass();
 
-    public HytalePluginTemplate(@Nonnull JavaPluginInit init) {
+    public HytaleTestPlugin(@Nonnull JavaPluginInit init) {
         super(init);
     }
 
