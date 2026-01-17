63b12f60a390b083c18a95f07555dabcb0b70b29
ProfiiDev <92174452+Profiidev@users.noreply.github.com>
2026-01-17 20:27:43 +0100

    feat: added button events

--- src/main/java/io/profidev/HytaleTestPlugin/ShopDialogPage.java
+++ src/main/java/io/profidev/HytaleTestPlugin/ShopDialogPage.java
@@ -5,12 +5,12 @@ import javax.annotation.Nonnull;
 import com.hypixel.hytale.codec.Codec;
 import com.hypixel.hytale.codec.KeyedCodec;
 import com.hypixel.hytale.codec.builder.BuilderCodec;
-import com.hypixel.hytale.codec.builder.BuilderCodec.Builder;
 import com.hypixel.hytale.component.Ref;
 import com.hypixel.hytale.component.Store;
 import com.hypixel.hytale.protocol.packets.interface_.CustomPageLifetime;
 import com.hypixel.hytale.protocol.packets.interface_.CustomUIEventBindingType;
-import com.hypixel.hytale.server.core.Message;
+import com.hypixel.hytale.protocol.packets.interface_.Page;
+import com.hypixel.hytale.server.core.entity.entities.Player;
 import com.hypixel.hytale.server.core.entity.entities.player.pages.InteractiveCustomUIPage;
 import com.hypixel.hytale.server.core.ui.builder.EventData;
 import com.hypixel.hytale.server.core.ui.builder.UICommandBuilder;
@@ -29,29 +29,59 @@ public class ShopDialogPage extends InteractiveCustomUIPage<ShopDialogPage.ShopD
       @Nonnull UIEventBuilder uiEventBuilder,
       @Nonnull Store<EntityStore> store) {
     uiCommandBuilder.append("Pages/ShopDialog.ui");
-    // uiEventBuilder.addEventBinding(CustomUIEventBindingType.Activating,
-    // "#GreetButton",
-    // new EventData().append("@PlayerName", "#NameInput.Value"));
+
+    uiEventBuilder.addEventBinding(CustomUIEventBindingType.SelectedTabChanged, "#MainTabs",
+        new EventData().append("@Data", "#MainTabs.SelectedTab").append("Action", "SwitchTab"));
+    uiEventBuilder.addEventBinding(CustomUIEventBindingType.Activating, "#CloseButton",
+        new EventData().append("Action", "ClosePage"));
   }
 
   @Override
   public void handleDataEvent(@Nonnull Ref<EntityStore> ref,
       @Nonnull Store<EntityStore> store, @Nonnull ShopDialogData data) {
-    var playerName = data.Action != null ? data.Action : "unknown";
-    playerRef.sendMessage(Message.raw("Hello, " + playerName + "! Welcome to the shop."));
+    if (data.action == null) {
+      return;
+    }
+
+    switch (data.action) {
+      case "SwitchTab":
+        UICommandBuilder builder = new UICommandBuilder();
+        builder.set("#DebugLabel.Text", "Switched to tab: " + data.data);
+        sendUpdate(builder);
+        break;
+      case "ClosePage":
+        var player = store.getComponent(ref, Player.getComponentType());
+        player.getPageManager().setPage(ref, store, Page.None);
+        break;
+    }
   }
 
   public static class ShopDialogData {
+    public String action;
+    public String data;
+
     @Nonnull
-    private static final Builder<ShopDialogData> BUILDER = BuilderCodec.builder(ShopDialogData.class,
-        ShopDialogData::new);
-    @Nonnull
-    public static final BuilderCodec<ShopDialogData> CODEC = BUILDER
-        .append(new KeyedCodec<>("@PlayerName", Codec.STRING), (d, v) -> {
-          d.Action = v;
-        }, (d) -> {
-          return d.Action;
-        }).add().build();
-    public String Action;
+    public static final BuilderCodec<ShopDialogData> CODEC;
+
+    static {
+      var builder = BuilderCodec.builder(ShopDialogData.class,
+          ShopDialogData::new);
+
+      var actionCodec = new KeyedCodec<>("Action", Codec.STRING);
+      builder.append(actionCodec, (d, v) -> {
+        d.action = v;
+      }, (d) -> {
+        return d.action;
+      }).add();
+
+      var dataCodec = new KeyedCodec<>("@Data", Codec.STRING);
+      builder.append(dataCodec, (d, v) -> {
+        d.data = v;
+      }, (d) -> {
+        return d.data;
+      }).add();
+
+      CODEC = builder.build();
+    }
   }
 }
